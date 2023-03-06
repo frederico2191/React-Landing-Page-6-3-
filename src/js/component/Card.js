2dd33b93df6916d3545155d4f74edf4ab4ba1cd7
Frederico Rodrigues <frederico2191@gmail.com>
2023-03-06 14:51:57 +0000

    props introduced in Cards

--- src/js/component/Card.js
+++ src/js/component/Card.js
@@ -1,24 +1,22 @@
 import React from "react";
 import image from '../../img/rigo-baby.jpg'
 
-const Card = () => {
+const Card = ({cardTitle, cardContent, cardUpdate}) => {
     // return (<div>hello! CARD!!</div>)
 
 
     return (
         <div>
         <div className="card mb-3 col-12">
-
-
-        <img src={image} className="card-img-top" alt="Image Alt"/>
+        <img src={image} className="card-img-top" alt="Image Alternative Description"/>
     <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+        <h5 className="card-title">{cardTitle}</h5>
+        <p className="card-text">{cardContent}</p>
+        <p className="card-text"><small className="text-muted">{cardUpdate}</small></p>
     </div>
     </div>
     </div>
     )
-    }
+}
 
 export default Card;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -10,15 +10,22 @@ import Footer from "./Footer"
 
 //create your first component
 const Home = () => {
+	const data = [
+		{id:1, cardTitle:'This is my title', cardContent: 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.', cardUpdate: 'Updated 3 minutes ago.'},
+		{id:2, cardTitle:'This is my 2nd title', cardContent: 'LALALLA 2 l lead-in to additional content. This content is a little bit longer.', cardUpdate: 'Updated 2 minutes ago.'},
+		{id:3, cardTitle:'This is my 3rd title', cardContent: 'This is a This is the fional of 3 cards that i have quickly invetedsd.', cardUpdate: 'Updated 5 minutes ago.'}
+	]
+
+
+	
 	return (
 		<div>
 			<Navbar/>
 			<Jumbotron/>
 			<div className="row row-cols-4" id="CardContainer">
-				<Card/>
-				<Card/>
-				<Card/>
-				<Card/>
+				{data.map(({ id, cardTitle, cardContent, cardUpdate })=>(
+          			<Card key={id} cardTitle={cardTitle} cardContent={cardContent} cardUpdate={cardUpdate}/>
+        		))}
 			</div>
 			<Footer/>
 		</div>
